9a1ddf9b63c8c24555b5598f2d26f468ab6a4b1c
Heyveryouaire <mathieu.pavy.29@gmail.com>
2021-04-09 14:54:39 +0200

    create addTeampage

--- script/init.js
+++ script/init.js
@@ -1,12 +1,16 @@
 import pages from "./loadPages.js"
 import loginComponent from "./components/login.js"
 import homeComponent from "./components/home.js"
+import addTeamComponent from "./components/addTeam.js"
+
+const { ipcRenderer } = require('electron')
 
 const main = document.getElementById("main")
 
 async function init(){
     // destructuring each pages
-    const { login_example, home } = await pages()
+    const { login_example, home, addTeam } = await pages()
+    console.log("ipceree", ipcRenderer)
     
     // default page :
     main.innerHTML = home
@@ -14,5 +18,6 @@ async function init(){
     // send the page as paramse
     homeComponent(home)
     loginComponent(login_example)
+    addTeamComponent(addTeam)
 
 }init()
